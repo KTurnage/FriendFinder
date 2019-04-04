54538daea1161caee960cc2308e4926f3d9bf14b
KTurnage <kristaturnage314@gmail.com>
2019-04-04 17:02:51 -0600

    added first friend to friends.js and apiRoutes updated

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsArray = [
+    {
+    name:"Ellen Degeneres",
+    photo:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn1.thr.com%2Fsites%2Fdefault%2Ffiles%2Fimagecache%2FNFE_portrait%2F2017%2F03%2Fellen_headshot_8.26-p_2017.jpg&imgrefurl=https%3A%2F%2Fwww.hollywoodreporter.com%2Flive-feed%2Fellen-degeneres-netflix-stand-up-special-1007166&docid=X4j5ELz3y6rQxM&tbnid=AuAoj7pC1bREmM%3A&vet=10ahUKEwiCnoHsxLfhAhUUyYMKHWS1A9YQMwhtKAQwBA..i&w=349&h=524&bih=729&biw=1431&q=ellen%20degeneres&ved=0ahUKEwiCnoHsxLfhAhUUyYMKHWS1A9YQMwhtKAQwBA&iact=mrc&uact=8",
+    scores:[5, 1, 3, 4, 5, 3, 5, 2, 5, 1]
+  }
+]
+
+module.exports = friendsArray;
+  
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+// Loading Data: linking to friends.js.
+var friends = require("../data/friends");
+
+// ROUTING
+module.exports = function(app) {
+
+app.get("/api/friends", function(req, res) {
+    res.json(friends);
+});
+
+app.post("/api/friends", function(req, res) {
+    res.json();
+});
+
+};
