585f919dd042acf8a7741f15731af3732051656a
KTurnage <kristaturnage314@gmail.com>
2019-04-07 13:15:24 -0600

    modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,30 @@ app.get("/api/friends", function(req, res) {
     res.json(friends);
 });
 
+  // API POST Requests
+  // Below code handles when a user clicks submit button after answering all questions.
+  // The answers are added and totaled. (loop through)
+  
+  // Compare user total to frieds array totals, and chose closest match. (loop through)
+  // After closest match is chosen, push the name and photo of friend to modal.
+  // In each of the below cases, when a user submits form data (a JSON object)
 app.post("/api/friends", function(req, res) {
-    res.json();
+friends.push(req.body);
+
+// variable for user score
+var userScore = 0;
+
+// use this object to hold the best friend match. This will constantly update as it loops thru options
+var bestFriendMatch = {
+    name:"",
+    photo:"",
+    friendDifference: infinity
+};
+
+// 
+var userData = req.body;
+var userScores = userData.scores;
+
 });
 
 };
